f802fa3ba8a77c43619e690e3bebc2b4a1f6adc8
James Wright <booff42@bigpond.com>
2017-02-02 17:08:21 +1100

    Initial simple-as-possible work.

--- equation_solver/__init__.py
+++ equation_solver/__init__.py
@@ -1,1 +1,30 @@
-def foo(): pass
+def evaluate(expression):
+    return int(eval(expression.split('=')[-1], {}, {}))
+
+
+class EquationParser:
+    def __init__():
+        pass
+
+    def parse():
+        pass
+
+
+class EquationNode:
+    pass
+
+
+class ExpressionNode:
+    pass
+
+
+class TermNode:
+    pass
+
+
+class NumberNode:
+    pass
+
+
+class UnknownNode:
+    pass
--- tests.py
+++ tests.py
@@ -1,1 +1,41 @@
-pass
+import unittest
+import equation_solver
+
+
+class TestExpression(unittest.TestCase):
+    def test_single_value(self):
+        value = equation_solver.evaluate('15')
+        self.assertEqual(value, 15)
+
+    def test_addition(self):
+        value = equation_solver.evaluate('2+3')
+        self.assertEqual(value, 5)
+
+    def test_subtraction(self):
+        value = equation_solver.evaluate('7-10')
+        self.assertEqual(value, -3)
+
+    def test_multiplication(self):
+        value = equation_solver.evaluate('6*9')
+        self.assertEqual(value, 54)
+
+    def test_division(self):
+        value = equation_solver.evaluate('-8/4')
+        self.assertEqual(value, -2)
+
+
+class TestEquation(unittest.TestCase):
+    def test_assignment(self):
+        value = equation_solver.evaluate('x=14*3')
+        self.assertEqual(value, 42)
+
+    def test_rearrangement(self):
+        value = equation_solver.evaluate('2=84/x')
+        self.assertEqual(value, 42)
+
+
+class TestMultipleEquations(unittest.TestCase):
+    pass
+
+
+unittest.main()
